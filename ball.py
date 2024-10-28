42af0703d55732c2d5a133563a89fadcc530d55a
Abstroar <abhilaksh2004sh@gmail.com>
2024-10-28 17:36:03 +0530

    Ball bounces and interacts with paddle and walls. paddle also move left and right with "a" and "right" key respectively

--- ball.py
+++ ball.py
@@ -5,11 +5,14 @@ class Ball(Turtle):
     def __init__(self):
         super().__init__()
         self.color("white")
-        self.speed(500)
+        self.penup()
+        self.speed("fastest")
+
         self.shape("circle")
         self.shapesize(stretch_wid=1, stretch_len=1)
-        self.x_move = 1
-        self.y_move = 1
+        self.x_move = 3
+        self.y_move = 3
+        self.move_speed = 0.01
 
     def xbounce(self):
         # current_heading = self.heading()
@@ -27,5 +30,6 @@ class Ball(Turtle):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
+        time.sleep(self.move_speed)
 
 
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ game_status = True
 screen.tracer(0)
 x = Ball()
 player = Paddle()
-
+tiles = []
 for i in range(-380, 390, 32):
     y = Tiles()
     yy = Tiles()
@@ -23,30 +23,42 @@ for i in range(-380, 390, 32):
     y.goto(i, 290)
     yy.goto(i + 5, 277)
     yyy.goto(i, 265)
+    tiles.append(y)
+    tiles.append(yy)
+    tiles.append(yyy)
 
 screen.listen()
 # screen.onkey(player.move_left, "w")
 screen.update()
 player.move_left()
-screen.onkey(player.move_left, "a")
+screen.onkey(player.move_left, "Left")
 screen.onkey(player.move_right, "Right")
 
-
-
+def gettiles(tar_x,tar_y,threshhold):
+    selected = []
+    for t in tiles:
+        if t.distance(tar_x, tar_y) < threshhold:
+            selected.append(t)
+            t.hideturtle()
+            t.clear()
+            return True
+            # print("hii")
 while game_status:
-
     screen.update()
     # print(x.position())
     if x.ycor() > 290:
         x.ybounce()
     elif x.xcor() > 390 or x.xcor() < -390:
         x.xbounce()
-
-    if x.distance(player) < 30 and x.ycor() > - 290:
+    elif x.distance(player) < 30 and x.ycor() > - 290:
         x.ybounce()
-
-
-
+    elif x.ycor() < -287:
+        print("game over")
+        game_status = False
+    elif gettiles(x.xcor(), x.ycor(), 10):
+        x.ybounce()
+    elif x.ycor() < -290:
+        game_status= False
     x.move()
 
 
